d2f2f75c7fb2258bf719800ae8907f83c44116fd
INOCcent-afk <68991068+INOCcent-afk@users.noreply.github.com>
2020-08-20 09:57:57 +0800

    liquid effect

---
+++ main.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./images/bm1.jpg",
+  image2: "./images/bm2.jpeg",
+  displacementImage: "./images/heightMap.png",
+});
